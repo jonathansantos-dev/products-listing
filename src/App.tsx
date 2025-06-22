34f3c48b71bc5c632669dc0b8998f9f3566d9e09
jonnyCSantos <jonathandossantos.c@gmail.com>
2025-06-22 11:01:21 -0300

    feat: revert some integrations

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,9 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import type { Product } from './services/types';
 import Products from './pages/products';
 
 export default function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [viewType, setViewType] = useState<'list' | 'grid' | 'table'>('list');
-
-  useEffect(() => {
-    // Aqui você vai chamar sua API e setar os produtos
-  }, []);
-
   return (
     <Router>      
       <Routes>
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,15 +1,7 @@
-// App.tsx
-import { useEffect, useState } from 'react';
-import type { Product } from '../../services/types';
 import * as S from './styles';
 import SidebarFilter from '../../components/SidebarFilter';
-import { useProducts } from '../../hooks/useProducts';
 
 export default function Products() {
-  const { products } = useProducts()
-  const [viewType, setViewType] = useState<'list' | 'grid' | 'table'>('list');
-
-  console.log('Products component called', products);
 
   return (
     <S.Container>
@@ -29,16 +21,11 @@ export default function Products() {
         {/* Botões de troca de visualização */}
         <div>
           <div>
-            <button onClick={() => setViewType('list')}>List</button>
-            <button onClick={() => setViewType('grid')}>Grid</button>
-            <button onClick={() => setViewType('table')}>Table</button>
+            <button onClick={() => null}>List</button>
+            <button onClick={() => null}>Grid</button>
+            <button onClick={() => null}>Table</button>
           </div>
           <section>
-            {products.map((product: Product, index: number) => {
-              return <li key={`${index}-${product.name}`}>
-                {product.name} - {product.price}
-              </li>
-            })}
           </section>
         </div>
       </S.Wrapper>
