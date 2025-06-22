dc0f13dd6fbcd0879ad94d46025e13291c45cfd4
wanderson <wandersonmj.dev@gmail.com>
2025-06-22 00:41:18 -0300

    fix: configure inital application template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-// App.tsx
-import { useEffect, useState } from 'react';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
-import Products from './pages/products';
 
 export default function App() {
   return (
     <Router>      
       <Routes>
         <Route path="/" element={<><h1>Minha Loja Virtual</h1> <Link to={"/products"}>Produtos</Link></>} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/products" element={<></>} />
       </Routes>      
     </Router>
   );
